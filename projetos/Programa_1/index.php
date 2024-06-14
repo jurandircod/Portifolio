d9c61582a13dc2d3af561c8afc70031bbda57e2b
jurandorcod <jurandiraparecido19651965@gmail.com>
2024-06-14 16:52:49 -0300

    1.1

--- projetos/Programa_1/index.php
+++ projetos/Programa_1/index.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://unpkg.com/scrollreveal"></script>
     <title>Upload de Fotos</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/Style.css">
---
+++ script.js
@@ -0,0 +1,14 @@
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.topo-do-site', { duration: 1000 });
+
+sr.reveal('.especialidades', { 
+    rotate: {x: 0, y: 80, z: 0},
+    duration: 2000 });
+
+sr.reveal('.sobre', { duration: 2000 });
+
+sr.reveal('.portfolio', { duration: 2000 });
+
+sr.reveal('.formulario', { duration: 2000 });
+
